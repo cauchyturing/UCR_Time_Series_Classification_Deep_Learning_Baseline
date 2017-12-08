01d2968ddd9f8783a118cedbd1d7d9b008b6110e
Oisín Moran <OisinMoran@users.noreply.github.com>
2017-12-08 10:43:19 +0000

    Import pandas
    Second last line currently throws "NameError: name 'pd' is not defined"

--- FCN.py
+++ FCN.py
@@ -14,6 +14,7 @@ import numpy as np
 
 import keras 
 from keras.callbacks import ReduceLROnPlateau
+import pandas as pd
       
 def readucr(filename):
     data = np.loadtxt(filename, delimiter = ',')
