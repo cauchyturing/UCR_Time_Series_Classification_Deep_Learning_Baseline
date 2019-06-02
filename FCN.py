312dbc5866bc761a6a468cd1d8fdaf910149eb2c
Zhiguang Wang <zgwang@fb.com>
2019-06-02 12:51:42 -0700

    up-to-date with tensorflow >= 1.13

--- FCN.py
+++ FCN.py
@@ -18,7 +18,7 @@ def readucr(filename):
     X = data[:,1:]
     return X, Y
   
-nb_epochs = 2000
+nb_epochs = 2
 
 
 #flist = ['Adiac', 'Beef', 'CBF', 'ChlorineConcentration', 'CinC_ECG_torso', 'Coffee', 'Cricket_X', 'Cricket_Y', 'Cricket_Z', 
@@ -84,3 +84,31 @@ for each in flist:
     #Print the testing results which has the lowest training loss.
     log = pd.DataFrame(hist.history)
     print(log.loc[log['loss'].idxmin]['loss'], log.loc[log['loss'].idxmin]['val_acc'])
+
+
+# %% Get CAM
+import matplotlib.pyplot as plt
+# from matplotlib.backends.backend_pdf import PdfPages
+
+get_last_conv = keras.backend.function([model.layers[0].input, keras.backend.learning_phase()], [model.layers[-3].output])
+last_conv = get_last_conv([x_test[:100], 1])[0]
+
+get_softmax = keras.backend.function([model.layers[0].input, keras.backend.learning_phase()], [model.layers[-1].output])
+softmax = get_softmax(([x_test[:100], 1]))[0]
+softmax_weight = model.get_weights()[-2]
+CAM = np.dot(last_conv, softmax_weight)
+
+
+# pp = PdfPages('CAM.pdf')
+for k in range(20):
+    CAM = (CAM - CAM.min(axis=1, keepdims=True)) / (CAM.max(axis=1, keepdims=True) - CAM.min(axis=1, keepdims=True))
+    c = np.exp(CAM) / np.sum(np.exp(CAM), axis=1, keepdims=True)
+    plt.figure(figsize=(13, 7));
+    plt.plot(x_test[k].squeeze());
+    plt.scatter(np.arange(len(x_test[k])), x_test[k].squeeze(), cmap='hot_r', c=c[k, :, :, int(y_test[k])].squeeze(), s=100);
+    plt.title(
+        'True label:' + str(y_test[k]) + '   likelihood of label ' + str(y_test[k]) + ': ' + str(softmax[k][int(y_test[k])]))
+    plt.colorbar();
+#     pp.savefig()
+#
+# pp.close()
